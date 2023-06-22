0684b059aeeb7cbc56ffd82a39831500ef6c3285
Bryan Chow <bchow.post@gmail.com>
2023-06-22 18:13:58 +0100

    Finish core functions
    Began adding dom functions

---
+++ src/core_modules.js
@@ -0,0 +1,79 @@
+import { format, formatDistanceToNowStrict, isFuture } from "date-fns";
+
+// Core functions of To Do
+
+// creates to do object with named properties
+export const createToDo = (
+	title,
+	description,
+	dueDate,
+	priority,
+	notes,
+	checklist,
+	complete = false,
+) => ({ title, description, dueDate, priority, notes, checklist, complete });
+
+// creates a project with a default to do for users to edit
+// returns an array which we can append to dos to
+export const createProject = () => {
+	const placeHolderToDo = createToDo(
+		"Create Your First To Do Now!",
+		"Include your description...",
+		"Set the due date...",
+		1,
+		"Add additional notes...",
+		{},
+	);
+	return [placeHolderToDo];
+};
+
+// sets completeness of to do item
+export const setComplete = (toDoItem) => {
+	// eslint-disable-next-line no-param-reassign
+	toDoItem.complete = !toDoItem.complete;
+};
+
+// sets priority of to do item
+export const setPriority = (toDoItem, newPriority) => {
+	toDoItem.priority = newPriority;
+};
+
+// changes state of checklist items
+export const setChecklist = (toDoItem, checklistItem) => {
+	if (toDoItem.checklist[checklistItem] !== undefined) {
+		toDoItem.checklist[checklistItem] = toDoItem.checklist[checklistItem]
+			? 0
+			: 1;
+	}
+};
+
+// adds checklist items
+export const addChecklistItem = (toDoItem, checklistItem) => {
+	if (toDoItem.checklist[checklistItem] === undefined) {
+		toDoItem.checklist[checklistItem] = 0;
+	} else {
+		return `${checklistItem} already exists`;
+	}
+};
+
+// combines date and time inputs to a date object
+export const combineDateTime = (dateInput, timeInput) => {
+	const dateTime = `${dateInput} ${timeInput}`;
+	return new Date(dateTime);
+};
+
+// returns a human readable date for displaying due date
+export const returnHumanDate = (combinedDateTime) => {
+	const date = format(combinedDateTime, "do MMM yyyy");
+	const time = format(combinedDateTime, "HH:mm");
+	return `${date} at ${time}`;
+};
+
+// returns a human readable duration for time to due date
+export const returnHumanDuration = (combinedDateTime) => {
+	const duration = formatDistanceToNowStrict(combinedDateTime);
+	if (isFuture(combinedDateTime)) {
+		return `Due in ${duration}`;
+	}
+	return `Overdue by ${duration}`;
+};
--- src/index.js
+++ src/index.js
@@ -1,57 +1,95 @@
-// Core functions of To Do
+import * as core from "./core_modules";
 
-// Creates to do object with named properties
-const createToDo = (
-	title,
-	description,
-	dueDate,
-	priority,
-	notes,
-	checklist,
-	complete = false,
-) => ({ title, description, dueDate, priority, notes, checklist, complete });
-
-// Creates a project with a default to do for users to edit
-// Returns an array which we can append to dos to
-const createProject = () => {
-	const placeHolderToDo = createToDo(
-		"Create Your First To Do Now!",
-		"Include your description...",
-		"Set the due date...",
-		1,
-		"Add additional notes...",
-		{},
-	);
-	return [placeHolderToDo];
+// create Input Function
+const createInput = (type, id, labelDisplay, divClass, placeHolder) => {
+	const inputDiv = document.createElement("div");
+	inputDiv.classList.add(divClass);
+	const label = document.createElement("label");
+	const input = document.createElement("input");
+	// set label attributes
+	label.setAttribute("for", id);
+	label.innerText = labelDisplay;
+	// set input attributes
+	input.setAttribute("type", type);
+	input.setAttribute("id", id);
+	input.setAttribute("name", id);
+	input.setAttribute("placeholder", placeHolder);
+	// return
+	inputDiv.appendChild(label);
+	inputDiv.appendChild(input);
+	return inputDiv;
 };
 
-// Sets completeness of to do item
-const setComplete = (toDoItem) => {
-	// eslint-disable-next-line no-param-reassign
-	toDoItem.complete = !toDoItem.complete;
+const createTextArea = (id, labelDisplay, divClass, placeHolder) => {
+	const textAreaDiv = document.createElement("div");
+	textAreaDiv.classList.add(divClass);
+	const label = document.createElement("label");
+	const textArea = document.createElement("textarea");
+	// set label attributes
+	label.setAttribute("for", id);
+	label.innerText = labelDisplay;
+	// set text area attributes
+	textArea.setAttribute("id", id);
+	textArea.setAttribute("name", id);
+	textArea.setAttribute("placeholder", placeHolder);
+	// return
+	textAreaDiv.appendChild(label);
+	textAreaDiv.appendChild(textArea);
+	return textAreaDiv;
 };
 
-// Sets priority of to do item
-const setPriority = (toDoItem, newPriority) => {
-	toDoItem.priority = newPriority;
+const createSlider = (id, labelDisplay, value, min, max, divClass) => {
+	const sliderDiv = document.createElement("div");
+	sliderDiv.classList.add(divClass);
+	const label = document.createElement("label");
+	const slider = document.createElement("input");
+	// set label attributes
+	label.setAttribute("for", id);
+	label.innerText = labelDisplay;
+	// set slider attributes
+	slider.setAttribute("type", "range");
+	slider.setAttribute("id", id);
+	slider.setAttribute("name", id);
+	slider.setAttribute("value", value);
+	slider.setAttribute("min", min);
+	slider.setAttribute("max", max);
+	// return
+	sliderDiv.appendChild(label);
+	sliderDiv.appendChild(slider);
+	return sliderDiv;
 };
 
-const setChecklist = (toDoItem, checklistItem) => {
-	if (toDoItem.checklist[checklistItem] !== undefined) {
-		toDoItem.checklist[checklistItem] = toDoItem.checklist[checklistItem]
-			? 0
-			: 1;
-	}
-};
+// create form inputs
+const toDoTitle = createInput(
+	"text",
+	"todo-title",
+	"What's the to do item?",
+	"todo-title-div",
+	"Buy Milk",
+);
 
-const addChecklistItem = (toDoItem, checklistItem) => {
-	if (toDoItem.checklist[checklistItem] === undefined) {
-		toDoItem.checklist[checklistItem] = 0;
-	} else {
-		return `${checklistItem} already exists`;
-	}
-};
+const toDoDesc = createTextArea(
+	"todo-desc",
+	"Description",
+	"todo-desc-div",
+	"Go to Lidl not Waitrose as the milk there is more expensive",
+);
 
-const defaultProject = createProject();
+const toDoDate = createInput("date", "todo-date", "Date", "todo-date-div");
+const toDoTime = createInput("time", "todo-time", "Time", "todo-time-div");
 
-console.log(defaultProject);
+const toDoPrior = createSlider(
+	"todo-prior",
+	"Set the priority",
+	3,
+	1,
+	5,
+	"todo-prior-div",
+);
+
+document.body.appendChild(toDoTitle);
+
+document.body.appendChild(toDoDesc);
+document.body.appendChild(toDoDate);
+document.body.appendChild(toDoTime);
+document.body.appendChild(toDoPrior);
