1e34c969215f7bed680c152bd95db54fa3f437a3
Bryan Chow <bchow.post@gmail.com>
2023-06-23 22:00:58 +0100

    Add checklist function

--- src/index.js
+++ src/index.js
@@ -62,11 +62,50 @@ const createSlider = (id, labelDisplay, value, min, max, divClass) => {
 	return sliderDiv;
 };
 
-const createCheckList = () => {
-	const checkListDiv = document.createElement("div");
-	const checkListTitle = document.createElement("div");
-	const checkListBox = document.createElement("");
-	checkListDiv.classList.add("check-list-item");
+const displayPriority = () => {
+	const priorityDiv = document.querySelector(".priority");
+	switch (Number(document.querySelector("#todo-prio").value)) {
+		case 1:
+			priorityDiv.innerText = "Lowest Priority";
+			break;
+		case 2:
+			priorityDiv.innerText = "Low Priority";
+			break;
+		case 3:
+			priorityDiv.innerText = "Normal Priority";
+			break;
+		case 4:
+			priorityDiv.innerText = "High Priority";
+			break;
+		case 5:
+			priorityDiv.innerText = "Highest Priority";
+			break;
+		default:
+			priorityDiv.innerText = "Normal Priority";
+			break;
+	}
+};
+
+const addChecklistItem = () => {
+	const checklist = document.querySelector(".checklist");
+	const checklistInput = document.querySelector("#add-checklist");
+	const checklistInputDiv = document.querySelector("#checklist-input-div");
+	if (/\S/.test(checklistInput.value)) {
+		const checklistItemWrapper = document.createElement("li");
+		const checklistItemLabel = document.createElement("label");
+		const checklistItemBox = document.createElement("input");
+		checklistItemLabel.setAttribute("for", checklistInput.value);
+		checklistItemLabel.innerText = checklistInput.value;
+		checklistItemBox.setAttribute("type", "checkbox");
+		checklistItemBox.setAttribute("id", checklistInput.value);
+		checklistItemBox.setAttribute("name", checklistInput.value);
+
+		checklistItemWrapper.appendChild(checklistItemLabel);
+		checklistItemWrapper.appendChild(checklistItemBox);
+
+		checklist.insertBefore(checklistItemWrapper, checklistInputDiv);
+	}
+	checklistInput.value = "";
 };
 
 // create form inputs
@@ -104,37 +143,40 @@ const priorityDisplay = (() => {
 	return priorityDiv;
 })();
 
+const addChecklist = (() => {
+	const checklistDiv = document.createElement("div");
+	const checklistInputDiv = document.createElement("div");
+	checklistInputDiv.setAttribute("id", "checklist-input-div");
+	const checklist = document.createElement("ul");
+	const checklistBtn = document.createElement("button");
+	checklistBtn.setAttribute("id", "checklist-btn");
+	checklistBtn.innerText = "+";
+	checklist.classList.add("checklist");
+	const checklistInput = createInput(
+		"text",
+		"add-checklist",
+		"Add a new checklist item",
+		"add-checklist-div",
+		"Milk",
+	);
+	checklistInputDiv.appendChild(checklistBtn);
+	checklistInputDiv.appendChild(checklistInput);
+	checklist.appendChild(checklistInputDiv);
+	checklistDiv.appendChild(checklist);
+
+	return checklistDiv;
+})();
+
 document.body.appendChild(toDoTitle);
 document.body.appendChild(toDoDesc);
 document.body.appendChild(toDoDate);
 document.body.appendChild(toDoTime);
 document.body.appendChild(toDoPrio);
 document.body.appendChild(priorityDisplay);
-
-const displayPriority = () => {
-	const priorityDiv = document.querySelector(".priority");
-	switch (Number(document.querySelector("#todo-prio").value)) {
-		case 1:
-			priorityDiv.innerText = "Lowest Priority";
-			break;
-		case 2:
-			priorityDiv.innerText = "Low Priority";
-			break;
-		case 3:
-			priorityDiv.innerText = "Normal Priority";
-			break;
-		case 4:
-			priorityDiv.innerText = "High Priority";
-			break;
-		case 5:
-			priorityDiv.innerText = "Highest Priority";
-			break;
-		default:
-			priorityDiv.innerText = "Normal Priority";
-			break;
-	}
-};
+document.body.appendChild(addChecklist);
 
 const prioSlider = document.querySelector("#todo-prio");
+const checklistBtn = document.querySelector("#checklist-btn");
 
 prioSlider.addEventListener("input", displayPriority);
+checklistBtn.addEventListener("click", addChecklistItem);
